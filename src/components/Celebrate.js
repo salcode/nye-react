d183e788018baf449852f0352dbbd1dd1abbbeb2
Sal Ferrarello <sal@ironcodestudio.com>
2018-12-29 13:23:14 -0500

    Add PropTypes to Celebrate component

--- src/components/Celebrate.js
+++ src/components/Celebrate.js
@@ -1,11 +1,19 @@
 import React from "react";
 import sizeMe from 'react-sizeme'
 import Confetti from 'react-confetti'
+import PropTypes from "prop-types";
 
 const Celebrate = sizeMe({
   monitorHeight: true,
   monitorWidth: true,
 })(class MyConfetti extends React.Component {
+  static propTypes = {
+    message: PropTypes.string,
+    size: PropTypes.shape({
+      height: PropTypes.number,
+      width:  PropTypes.number,
+    }),
+  };
   render() {
     return (
       <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
