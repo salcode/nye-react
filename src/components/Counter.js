0a451cf4791358cf9cf2a76b9834c9b8b7eeac33
Sal Ferrarello <sal@ironcodestudio.com>
2018-12-29 13:07:57 -0500

    Add PropTypes to Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class Counter extends React.Component {
+  static propTypes = {
+    minutes: PropTypes.number,
+    seconds: PropTypes.number,
+  };
   addLeadingZero = (num) => {
     if (num<10) {
       return '0' + num;
