9eb7c6802132540fbefa5aa45cbc2624fe47e31e
Sal Ferrarello <sal@ironcodestudio.com>
2018-12-31 09:28:24 -0500

    Add PropTypes to Background component

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,6 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class Background extends React.Component {
+	static propTypes = {
+		children: PropTypes.any,
+		image: PropTypes.string,
+		locationIndex: PropTypes.number,
+	};
   render() {
     return (
       <div className="background" style={{backgroundImage: `url(${this.props.image})`}}>
